875357f34f822dd2939a954f2fdef095b574502d
aleng1106 <ltlobygg@163.com>
2024-11-29 11:04:41 +0800

    init

---
+++ app/dashboard/about/page.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Image from "next/image";
+
+export default function Home1() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      about
+    </div>
+  );
+}
--- app/dashboard/template.tsx
+++ app/dashboard/template.tsx
@@ -1,15 +1,22 @@
-import { Fragment } from "react";
+"use client";
+import { Fragment, useState } from "react";
 
-export default function DashboardLayout({
+export default function DashboardTemplate({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [count, setCount] = useState(0);
   return (
     <Fragment>
-      template
-      <br />
-      {children}
+      <div>
+        <h2>template: {count}</h2>
+        <button className="test" onClick={() => setCount(count + 1)}>
+          increment
+        </button>
+        <br />
+        {children}
+      </div>
     </Fragment>
   );
 }
